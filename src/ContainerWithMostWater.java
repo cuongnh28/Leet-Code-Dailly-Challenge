41a5a5929adad6c49cfb38f17f0b49b60eafce4e
cuongnh28 <you@example.com>
2022-10-14 09:57:34 +0700

    Container With Most Water

---
+++ src/ContainerWithMostWater.java
@@ -0,0 +1,31 @@
+public class ContainerWithMostWater {
+    public static void main(String[] args) {
+        int [] arr = {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(arr));
+    }
+
+    public static int maxArea(int[] height) {
+        int left = 0,
+            right = height.length - 1,
+            maxArea = 0;
+        while (left < right) {
+            int heightLeft = height[left];
+            int heightRight = height[right];
+            int h = Math.min(heightLeft, heightRight);
+            int w = right - left;
+            int currentArea = h * w;
+            if (heightLeft < heightRight) {
+                left++;
+            }
+            else if (heightLeft > heightRight) {
+                right--;
+            }
+            else {
+                left++;
+                right--;
+            }
+            maxArea = Math.max(maxArea, currentArea);
+        }
+        return maxArea;
+    }
+}
