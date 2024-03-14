f7ef8b52fe1bb19cca489e0ba01d6854a1c0c076
cuongnh28 <hongcuongcl98@gmail.com>
2024-03-14 14:08:05 +0700

    leet code 75 start

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+O
+src/LeetCode75_MergeString.java,1/e/1e4930430cb6df6847e70bfc60ae4c60e675a26d
+`
+0src/Find_Numbers_with_Even_Number_of_Digits.java,c/1/c162bbe968a752ec716ea69e7190733106f6464f
+N
+src/ReverseWordsInAString.java,a/7/a753a978ba00026a8e49a42bedafcf321cee8cfc
---
+++ src/ReverseWordsInAString.java
@@ -0,0 +1,16 @@
+public class ReverseWordsInAString {
+    public static void main(String[] args) {
+        String s = "the sky is blue";
+        System.out.println(reverseWords(s));
+    }
+
+    public static String reverseWords(String s) {
+        StringBuilder result = new StringBuilder();
+        String[] str = s.trim().split("\\s+");
+        for (int i = str.length - 1; i > 0; i--) {
+            result.append(str[i]).append(" ");
+        }
+        result.append(str[0]);
+        return result.toString();
+    }
+}
